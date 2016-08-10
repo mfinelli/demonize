24e6a6ac408504c9cb61537476cbd06ab4dc8fc1
Mario Finelli <mario@finel.li>
2016-08-10 19:25:08 -0400

    Daemonize the process if the option is passed

--- lib/demonize/server.rb
+++ lib/demonize/server.rb
@@ -1,6 +1,12 @@
 module Demonize
   class Server
+    def initialize(options={})
+      @daemonize = options.fetch(:daemonize, true)
+    end
+
     def run!(&block)
+      Process.daemon if @daemonize
+
       loop do
         yield block
       end
